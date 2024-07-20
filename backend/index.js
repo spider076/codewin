826e3f6481b55651c823bd11c355ba928dba86e3
spider076 <saadabban76@gmail.com>
2024-07-20 14:48:36 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,10 @@ import express from 'express';
 
 const app = express();
 
-
 app.get('/', (req,res)=>{
 	console.log('hi there !');
-}
+});
 
 app.listen(3000, ()=>{
 	console.log('server is running on port 3000');
-}
+});
