3f42b3b019c49e4fab6bcc3d0af7d693b1322986
spider076 <saadabban76@gmail.com>
2024-07-20 14:58:58 +0530

    minor one

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,6 @@ app.get('/', (req,res)=>{
 	console.log('hi there !');
 });
 
-app.listen(3000, ()=>{
-	console.log('server is running on port 3000');
+app.listen(3001, ()=>{
+	console.log('server is running on port 3001');
 });
